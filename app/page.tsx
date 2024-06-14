eecbae0d888624e087b090e33b07f8dd1dac5ba8
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2024-06-14 01:43:23 +0000

    Changes made to splashscreen

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,17 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-white text-black">
-      <div className="flex items-center justify-center">
+      <div className="max-w-screen-md mx-auto px-4">
+        {/* Centering and adjusting max width */}
         <div className="logo-container flex flex-col items-center">
           <div className="mb-4">
             <Image src="/Logo.svg" alt="Logo" width={120} height={120} />
           </div>
-          <div className="logo-text flex flex-row items-center">
-            <p className="text-black font-normal text-xl font-Poppins">Little</p>
-            <p className="text-purple-600 font-medium text-4xl leading-tight ml-1 font-Poppins">S</p>
-            <p className="text-black font-normal text-xl font-Poppins">tockpile</p>
+          <div className="logo-text flex items-center">
+            {/* Adjusted layout for responsiveness */}
+            <p className="text-black font-normal text-lg font-Poppins mr-1">Little</p>
+            <p className="text-purple-600 font-medium text-4xl leading-tight font-Poppins">S</p>
+            <p className="text-black font-normal text-lg font-Poppins">tockpile</p>
           </div>
         </div>
       </div>
