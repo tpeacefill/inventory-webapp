46f859d12ba5627815aa8deb616c6dcaeeca26c6
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2024-06-15 16:39:44 +0000

    UI complete for Login page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,22 @@
-// LoginPage.js
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import '../auth/page.css';
 import ImageLeft from './ImageLeft';
 
-const LoginPage = () => {
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const clearEmail = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault(); // Prevent default behavior
+    setEmail('');
+  };
+
+  const clearPassword = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault(); // Prevent default behavior
+    setPassword('');
+  };
+
   return (
     <div className='bg-white min-h-screen w-screen flex justify-center items-center'>
       <div className='min-h-[80vh] w-11/12 lg:w-3/4 flex flex-col lg:flex-row justify-between h-full'>
@@ -23,11 +36,43 @@ const LoginPage = () => {
                 <h1 className='text-purple-500 font-Poppins text-2xl font-medium mb-6'>Log into your account</h1>
                 <div className="mb-4">
                   <label className='block text-purple-500 text-lg font-light mb-2 font-Poppins' htmlFor="email">Enter email</label>
-                  <input type="email" id="email" className="w-full h-12 rounded border border-gray-300 bg-transparent text-gray-500 px-2 py-1 font-Poppins" placeholder='Enter your email'/>
+                  <div className='input-wrapper'>
+                    <input
+                      type="email"
+                      id="email"
+                      className="input-field rounded border border-gray-300 bg-transparent text-gray-500 px-2 py-1 font-Poppins"
+                      placeholder='Enter your email'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <div className='input-icon' onMouseDown={clearEmail}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                        <path d="M9 16.5C13.1421 16.5 16.5 13.1421 16.5 9C16.5 4.85786 13.1421 1.5 9 1.5C4.85786 1.5 1.5 4.85786 1.5 9C1.5 13.1421 4.85786 16.5 9 16.5Z" fill="#6E6D7A"/>
+                        <path d="M11.25 6.75L6.75 11.25" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                        <path d="M6.75 6.75L11.25 11.25" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                      </svg>
+                    </div>
+                  </div>
                 </div>
                 <div className="mb-4">
                   <label className='block text-purple-500 text-lg font-light mb-2 font-Poppins' htmlFor="password">Enter password</label>
-                  <input type="password" id="password" className="w-full h-12 rounded border border-gray-300 bg-transparent text-gray-500 px-2 py-1 font-Poppins" placeholder='Enter your password'/>
+                  <div className='input-wrapper'>
+                    <input
+                      type="password"
+                      id="password"
+                      className="input-field rounded border border-gray-300 bg-transparent text-gray-500 px-2 py-1 font-Poppins"
+                      placeholder='Enter your password'
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <div className='input-icon' onMouseDown={clearPassword}>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                        <path d="M9 16.5C13.1421 16.5 16.5 13.1421 16.5 9C16.5 4.85786 13.1421 1.5 9 1.5C4.85786 1.5 1.5 4.85786 1.5 9C1.5 13.1421 4.85786 16.5 9 16.5Z" fill="#6E6D7A"/>
+                        <path d="M11.25 6.75L6.75 11.25" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                        <path d="M6.75 6.75L11.25 11.25" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                      </svg>
+                    </div>
+                  </div>
                 </div>
                 <div className="w-full flex justify-end mb-4">
                   <a href="#" className='text-purple-500 text-base font-medium font-Poppins'>Forgot password?</a>
