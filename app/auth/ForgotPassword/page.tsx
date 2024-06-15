a1d4cb1b4f2399f990b04cd625b91fe2a0262e27
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2024-06-15 17:24:38 +0000

    Created a forgot password page

---
+++ app/auth/ForgotPassword/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../components/ImageLeft';
+import ImageLeft from '../../components/ImageLeft';
+
+const ForgotPassword = () => {
+  return (
+    <div className='bg-white min-h-screen w-screen flex justify-center items-center'>
+      <div className='min-h-[80vh] w-11/12 lg:w-3/4 flex flex-col lg:flex-row justify-between h-full'>
+        <ImageLeft />
+      </div>
+    </div>
+  )
+}
+
+export default ForgotPassword
