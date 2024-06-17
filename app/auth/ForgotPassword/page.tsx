3134ea6332a97142f31c3e200c037641c8c2e696
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2024-06-17 01:07:08 +0000

    Completed UI for Forgot Password page

--- app/auth/ForgotPassword/page.tsx
+++ app/auth/ForgotPassword/page.tsx
@@ -19,6 +19,7 @@ const ForgotPassword: React.FC = () => {
                 <InputField label="Enter email" type="email" placeholder="Enter your email" />
                 <div className="w-full flex justify-end mb-4">
                 </div>
+                <p className='font-Poppins text-base font-light text-purple-500 mb-4'>Enter email associated with account</p>
                 <button className="w-full h-12 flex justify-center items-center text-white bg-purple-500 rounded text-lg font-normal font-Poppins">Log In</button>
               </div>
             </div>
