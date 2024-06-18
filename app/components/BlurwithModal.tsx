60d0d1c2fa5a622ac78f10e8e00af00b0a8a4e8f
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2024-06-18 17:21:09 +0000

    Responsiveness for BlurwithModal sorted out and complete

--- app/components/BlurwithModal.tsx
+++ app/components/BlurwithModal.tsx
@@ -10,8 +10,8 @@ interface BlurWithModalProps {
 
 const BlurWithModal: React.FC<BlurWithModalProps> = ({ svg, title, message, href }) => {
   return (
-    <div className="fixed inset-0 flex justify-end items-center z-20">
-      <div className="w-1/2 h-full bg-custom-gray backdrop-blur-custom flex justify-center items-center">
+    <div className="fixed inset-0 flex justify-end lg:justify-flex-end items-center z-20">
+      <div className="w-full lg:w-1/2 h-full bg-custom-gray backdrop-blur-custom flex justify-center items-center">
         <div className="w-11/12 sm:w-2/5 max-w-md p-4 bg-white rounded-xl shadow-lg">
           <CustomModal
             svg={svg}
