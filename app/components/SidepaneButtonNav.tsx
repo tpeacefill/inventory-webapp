88f1f30a1a307117acd94a786f3bb85f041f4653
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2024-06-19 16:56:50 +0000

    SidepaneButtonNav has all widths equal

--- app/components/SidepaneButtonNav.tsx
+++ app/components/SidepaneButtonNav.tsx
@@ -8,7 +8,7 @@ type SidepaneButtonProps = {
 
 const SidepaneButton: React.FC<SidepaneButtonProps> = ({ icon, text, className }) => {
   return (
-    <button className={`flex gap-4 px-10 py-2.5 text-base whitespace-nowrap rounded bg-purple-500 bg-opacity-30 mb-5 ${className}`}>
+    <button className={`flex gap-4 px-10 py-2.5 text-base whitespace-nowrap rounded bg-purple-500 bg-opacity-30 mb-5 w-[198.45px] ${className}`}>
       {icon}
       <p className='font-poppins text-black text-[15.75px] font-normal leading-normal'>{text}</p>
     </button>
