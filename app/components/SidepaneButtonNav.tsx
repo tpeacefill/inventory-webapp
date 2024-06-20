5604a155b36df691b73e177c9bc6fd223834ee5e
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2024-06-20 13:25:09 +0000

    SidepaneButtonNav states defined

--- app/components/SidepaneButtonNav.tsx
+++ app/components/SidepaneButtonNav.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 
 type SidepaneButtonProps = {
   icon: React.ReactNode;
@@ -7,8 +8,17 @@ type SidepaneButtonProps = {
 };
 
 const SidepaneButton: React.FC<SidepaneButtonProps> = ({ icon, text, className }) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive); // Toggle the isActive state
+  };
+
   return (
-    <button className={`flex gap-4 px-10 py-2.5 text-base whitespace-nowrap rounded bg-purple-500 bg-opacity-30 mb-5 w-[198.45px] ${className}`}>
+    <button
+      className={`flex gap-4 px-10 py-2.5 text-base whitespace-nowrap rounded mb-5 w-[198.45px] ${isActive ? 'bg-purple-500 bg-opacity-30' : 'bg-transparent'} ${className}`}
+      onClick={handleClick} // Handle click to toggle isActive
+    >
       {icon}
       <p className='font-poppins text-black text-[15.75px] font-normal leading-normal'>{text}</p>
     </button>
