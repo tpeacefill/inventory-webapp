4b3fdd590e7f76c496f6ffbf4936ab10df1f2f53
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2024-06-27 18:27:14 +0000

    Created all the pages

--- app/Categories/page.tsx
+++ app/Categories/page.tsx
@@ -1,22 +1,19 @@
 import React from "react";
-import Menubar from "../components/Menubar";
 import Sidepanel from "../components/Sidepanel";
-import Image from "next/image";
+import Menubar from "../components/Menubar";
 
-const Products = () => {
+const Categories = () => {
   return (
     <div className="flex flex-col h-screen w-screen bg-zinc-50">
       <Menubar />
       <div className="flex flex-grow h-full w-full">
         <Sidepanel />
         <div className="flex-grow bg-gray-100 p-2  flex flex-col flex-shrink">
-          <div className="flex gap-5 max-md:flex-col max-md:gap-0 1265px:flex-col overflow-auto">
-        
-          </div>
+          <div className="flex gap-5 max-md:flex-col max-md:gap-0 1265px:flex-col overflow-auto"></div>
         </div>
       </div>
     </div>
   );
 };
 
-export default Products;
+export default Categories;
--- app/Settings/page.tsx
+++ app/Settings/page.tsx
@@ -3,7 +3,7 @@ import Menubar from "../components/Menubar";
 import Sidepanel from "../components/Sidepanel";
 import Image from "next/image";
 
-const Products = () => {
+const Settings = () => {
   return (
     <div className="flex flex-col h-screen w-screen bg-zinc-50">
       <Menubar />
@@ -19,4 +19,4 @@ const Products = () => {
   );
 };
 
-export default Products;
+export default Settings;
